477389bab861f732b6d36ff963d1684335e33e24
ubansi <goakafu@gmail.com>
2019-03-21 21:18:05 +0900

    変数名の修正

--- keyboards/lily58/lib/uptime_render.c
+++ keyboards/lily58/lib/uptime_render.c
@@ -4,10 +4,11 @@
 char timelog_str[24] = {};
 
 void set_timelog(void){
-    uint32_t last_time = timer_read32();
-    int seconds = (int)(last_time / 1000 ) % 60;
-    int minutes = (int)(last_time / 1000 / 60 ) % 60;
-    int hour = (int)(last_time / 1000 / 60 / 60);
+    uint32_t uptime_millsec = timer_read32();
+    uint32_t uptime_sec = uptime_millsec / 1000;
+    int seconds = (int)uptime_sec % 60;
+    int minutes = (int)(uptime_sec / 60 ) % 60;
+    int hour = (int)(uptime_sec / 60 / 60);
     snprintf(timelog_str, sizeof(timelog_str), "uptime: %02d:%02d:%02d", hour, minutes, seconds);
 }
 
