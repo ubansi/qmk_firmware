63aebe3e930abc0ddc058b0d94f392b0bd0bdae6
ubansi <goakafu@gmail.com>
2019-03-24 00:12:54 +0900

    入力数カウンタの型をuint32_tに変更

--- keyboards/lily58/lib/typecount_render.c
+++ keyboards/lily58/lib/typecount_render.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "lily58.h"
 
-int type_count = 0;
+uint32_t type_count = 0;
 char countlog_str[24] = {};
 
 void count_up(void){
     type_count++;
-    snprintf(countlog_str, sizeof(countlog_str),"Type Count: %5d",type_count);
+    snprintf(countlog_str, sizeof(countlog_str),"Type Count: %"PRIx32"",type_count);
 }
 
 const char *read_countlog(void) {
