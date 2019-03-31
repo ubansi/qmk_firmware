ec6da12dcd48a679310f21e516c6f97cbd7dc4cb
ubansi <goakafu@gmail.com>
2019-03-31 17:24:34 +0900

    表示がオーバーフローするバグの修正

--- keyboards/lily58/lib/uptime_render.c
+++ keyboards/lily58/lib/uptime_render.c
@@ -6,8 +6,8 @@ char timelog_str[24] = {};
 void set_timelog(void){
     uint32_t uptime_millsec = timer_read32();
     uint32_t uptime_sec = uptime_millsec / 1000;
-    int seconds = (int)uptime_sec % 60;
-    int minutes = (int)(uptime_sec / 60 ) % 60;
+    int seconds = (int)(uptime_sec % 60);
+    int minutes = (int)(uptime_sec / 60 % 60);
     int hour = (int)(uptime_sec / 60 / 60);
     snprintf(timelog_str, sizeof(timelog_str), "uptime: %02d:%02d:%02d", hour, minutes, seconds);
 }
